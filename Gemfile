4a8e98002c69c922bb954100bd4a20d1dbf80283
Derek Bender <170351+djbender@users.noreply.github.com>
2018-03-12 23:23:15 -0500

    scraper proof of concept

--- Gemfile
+++ Gemfile
@@ -4,3 +4,6 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # Specify your gem's dependencies in motocross_gear.gemspec
 gemspec
+
+local_gemfile = File.expand_path("Gemfile-local.rb", __dir__)
+eval_gemfile(local_gemfile) if File.readable?(local_gemfile)
--- lib/motocross_gear.rb
+++ lib/motocross_gear.rb
@@ -1,5 +1,51 @@
-require "motocross_gear/version"
+begin
+  require 'awesome_print'
+rescue LoadError
+end
 
+require 'nokogiri'
+require 'faraday'
+require 'motocross_gear/version'
+
+$doc = nil
 module MotocrossGear
-  # Your code goes here...
+  # catalog of websites
+  # websites have kits
+  # kits have pictures, price, (availability? colorways?)
+  require 'ostruct'
+  Kit = Struct.new(:name, :image, :url)
+  THORMX = {
+    root: 'http://www.thormx.com',
+    kits: '/products/?categoryId=7075'
+  }
+  def self.run
+    connection = Faraday.new(url: THORMX.fetch(:root))
+    response = connection.get(THORMX.fetch(:kits))
+    $doc = Nokogiri::HTML(response.body)
+    #ap $doc
+
+    $doc.css('#content').first.css('.productImage').map do |product|
+      name = product.css('.productNameText').first.text.to_s
+      image = product.css('img').first.attributes["src"].to_s
+      url = product.css('a').first.attributes["href"].to_s
+      Kit.new(name, image, url)
+    end
+
+    #entries = doc.css('.lagos-market-rates-inner')
+  end
+
+  def self.reload!
+    (MotocrossGear.constants - [:VERSION]).each do |const|
+      remove_const(const)
+    end
+    load 'motocross_gear.rb'
+  end
+end
+
+def reload!
+  MotocrossGear.reload!
+end
+
+def run
+  MotocrossGear.run
 end
